6d6ae4e488220b99e4be425929dd76fc835871f6
uvshrivass <uvshrivas@gmail.com>
2019-03-05 10:24:47 +0530

    React-crash

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = ()=>{
+    return <h1>I am from Header component.</h1>
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/header"
+
+const App = ()=>{
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
