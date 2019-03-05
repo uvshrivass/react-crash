5ecddcdb5fc7ca2556e2477eca58028aa9670dab
uvshrivass <uvshrivas@gmail.com>
2019-03-05 10:58:50 +0530

    Update with class

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,32 @@
 import React from "react";
 
+// Declaring variable that will can be accessed later
 const info = {
     fname : "Ujjwal",
     lname : "Shrivas"
 }
 
-const Header = ()=>{
-    return <h1>I am from Header component. Your first name is {info.fname} and your last name is {info.lname}.</h1>
-}
 
+// Using ES5 method, i.e without using class
+// const Header = ()=>{
+//     return (
+//     <div>
+//     <h1>I am from Header component. </h1>
+//     <p>Your first name is {info.fname} and your last name is {info.lname}.</p>
+//     </div>
+//     )
+// }
+
+
+// Using ES6 method, i.e using class
+class Header  extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>I am from Header component. </h1>
+            <p>Your first name is {info.fname} and your last name is {info.lname}.</p>
+            </div>
+        )
+    }
+}
 export default Header;
