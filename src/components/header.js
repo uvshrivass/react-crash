98badc6261f2898a124d2eac1f2d0feb06be9e11
uvshrivass <uvshrivas@gmail.com>
2019-03-05 10:35:30 +0530

    Updated

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,12 @@
 import React from "react";
 
+const info = {
+    fname : "Ujjwal",
+    lname : "Shrivas"
+}
+
 const Header = ()=>{
-    return <h1>I am from Header component.</h1>
+    return <h1>I am from Header component. Your first name is {info.fname} and your last name is {info.lname}.</h1>
 }
 
 export default Header;
